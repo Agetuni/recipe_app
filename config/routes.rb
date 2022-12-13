f7333e512a7efea2beb969fb9a98bc9603a4c0b9
Aleazar yilma <yilma4060@gmail.com>
2022-12-13 19:50:18 +0300

    add recipe crud operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "publics#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :recipes, only: [:index, :show, :new, :destroy, :create, :update] do
+    resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
+  end
+  get "/shopping_list", to: "foods#shopping_list"
 end
