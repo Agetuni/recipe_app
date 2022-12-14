4f3397fec935af46218a4c665e0ef44d60651051
Tadesse Jemal <tadessejemal63@gmail.com>
2022-12-14 18:32:51 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "public_recipes#index"
-=======
-  root "publics#index"
  
   resources :recipes, only: [:index, :show, :new, :destroy, :create, :update] do
     resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
@@ -14,6 +12,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-=======
+
   get "/shopping_list", to: "foods#shopping_list"
 end
